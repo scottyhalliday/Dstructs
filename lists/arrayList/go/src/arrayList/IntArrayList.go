d63e5feefb81f9931f6ffc68cdfb993760e49f92
Scott Hall <smhall316@gmail.com>
2018-10-24 07:25:16 -0400

    Added framework for go implementation of array list

---
+++ lists/arrayList/go/src/arrayList/IntArrayList.go
@@ -0,0 +1,7 @@
+package arrayList
+
+import "fmt"
+
+func IntArrayList() {
+  fmt.Printf("CALLED AN INTARRAYLIST\n")
+}
---
+++ lists/arrayList/go/src/test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "arrayList"
+)
+
+func main() {
+  fmt.Println("In main and imported ArrayList")
+  arrayList.IntArrayList()
+}
